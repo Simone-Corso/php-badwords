e86cd8069fc023106431c1e81578cc9336984e3b
Simone-Corso <scorso26@gmail.com>
2024-01-12 16:27:54 +0100

    ora stampa la funziona, facendo delle ricerche, dovevo mettere include per lo script.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,40 +8,46 @@
 <body>
 
 
-<?php
-//andrò a realizzare una variabile con un paragrafo di testo che desidero io
+<style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 700px;
+        }
 
+        .apple {
+            text-align: center;
+        }
+    </style>
+
+<?php include 'script.php'; ?>
+
+<div class="apple">
+<?php
+
+
+
+//andrò a realizzare una variabile con un paragrafo di testo che desidero io
 $testo = "Apple è la miglior azienda al mondo per la sua affidabilità nel mercato tecnologico.";
 
 //stampare nello schermo il paragrafo e la sua lunghezza
-
-
 echo $testo . "<br>";
-echo "La sua lunghezza è:" . strlen($testo);
-
+echo "La sua lunghezza è:" . strlen($testo) . "<br>";
 
 //Andrò a realizzare la parola censurata dal GET 
-
-$parolaDaCensurare = isset($_GET ['parola']) ? $_GET['parola'] : 'Apple';
-
-
-
-echo "Parola da censurare è: $parolaDaCensurare<br>" ;
+$parolaDaCensurare = isset($_GET['parola']) ? $_GET['parola'] : 'Apple';
+echo "Parola da censurare è: $parolaDaCensurare<br>";
 
 //Richiamare la funzione che ho creato nel script.php 
-
-$testoCensurato = Apple($testo, $parolaDaCensurare);
+$testoCensurato = censuraParola($testo, $parolaDaCensurare);
 
 //Come dice nell'esercizio di stampare nuovamente con la parola censurata
-
-echo "Paragrafo censurato è:" ;
-
-echo $testpCensurato ;
-
-echo "La lunghezza è : " . strlen($testoCensurato) ;
-
-
+echo "Paragrafo censurato è:" . $testoCensurato . "<br>";
+echo "La lunghezza è: " . strlen($testoCensurato) . "<br>";
 ?>
+</div>
+
     
 </body>
 </html>
--- script.php
+++ script.php
@@ -1,11 +1,11 @@
 <!-- dovrò realizzare una function per censurare la parola !-->
 
 <?php
+// dovrò realizzare una function per censurare la parola
 
 function censuraParola($testo, $parolaDaCensurare) {
     $testoCensurato = str_ireplace($parolaDaCensurare, '***', $testo);
     return $testoCensurato;
 }
-
 ?>
 
