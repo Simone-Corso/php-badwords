bca82044ec12f4d3f8d0a18bb981463b2fc1fe3f
Simone-Corso <scorso26@gmail.com>
2024-01-12 15:51:12 +0100

    ho messo una funzione per censurare la parola

---
+++ script.php
@@ -0,0 +1,12 @@
+<!-- dovrò realizzare una function per censurare la parola !-->
+
+<?php
+
+function censuraParola($testo, $parolaDaCensurare) {
+    $testoCensurato = str_ireplace($parolaDaCensurare, '***', $testo) ;
+
+    return $testoCensurato;
+}
+
+?>
+
